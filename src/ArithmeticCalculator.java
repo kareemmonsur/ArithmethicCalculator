3f06fa87ba8505b4649c0f1ca949975ee1fc127d
monsur kareem <monsurkareem@gmail.com>
2023-11-03 20:14:43 +0000

    Initial commit

---
+++ src/ArithmeticCalculator.java
@@ -0,0 +1,16 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class ArithmeticCalculator {
+    public static void main(String[] args) {
+        Addition(4,8);
+
+        }
+
+    public static int Addition() {
+            return Addition(0, 0);
+        }
+
+    public static int Addition(int firstNumber, int secondNumber ){
+        return firstNumber + secondNumber;
+        }
+    }
